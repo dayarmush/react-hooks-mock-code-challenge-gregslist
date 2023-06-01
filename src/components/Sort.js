4d92fdee52fc2e698e8cac09b677a98ede296cef
dovid yarmush <yarmushda@gmail.com>
2023-06-01 12:39:13 -0400

    minor updates

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -1,7 +1,11 @@
 function Sort({ isChecked, updateCheck}) {
     return (
     <label>
-      <input type="checkbox" checked={isChecked} onChange={updateCheck}/>
+      <input type="checkbox"
+      name='checkbox'
+       checked={isChecked} 
+       onChange={updateCheck}
+      />
       <span>Sort Alphabetically</span>
      </label>
     )
